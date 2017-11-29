d84ed91454631d5f91135e59b29a76c668fbb83b
grubdragon <aryanagal98@gmail.com>
2017-11-29 20:17:26 +0530

    Switched around some code under question fetching API

--- routes/questions.js
+++ routes/questions.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var sha3 = require('js-sha3');
+var crypto = require('crypto');
 var router = express.Router();
 var monk = require('monk');
 var db = monk('localhost:27017/treasure');
@@ -8,7 +8,7 @@ var db = monk('localhost:27017/treasure');
 router.put('/', function(req, res, next) {
   res.render('index', { title: 'Treasure Hunt' });
 });
-
+/*
 router.put('/:id', function(req, res){
     var collection = db.get('questions');
     collection.update({
@@ -23,26 +23,27 @@ router.put('/:id', function(req, res){
         res.json(video)
     });
 });
-
+*/
 
 router.get('/:id', function(req, res) {
     var userdb = db.get('users');
     var questiondb = db.get('questions');
-    var levelReq = req.params.id;
+    var levelReq = req.query['levelReq'];
     
-    var x = req.body.firstName;
-    var y = req.body.fbid;
-    var z = req.body.lastName
-    var sha3req = sha3.sha3_512(x+y+z);
-    
-    userdb.findOne({ hash : sha3req }, function(err, usr){
+    var x = req.query['firstName'];
+    var y = req.query['fbid'];
+    var z = req.query['lastName'];
+    var md5req = crypto.createHash('md5').update(x+y+z).digest('hex');
+
+    userdb.findOne({ hash : md5req }, function(err, usr){
         if (err) throw err;
+        console.log("usr: "+usr);
         if(isNaN(levelReq)){
-    		levelReq = usr.currLvl
+    		levelReq = usr.currLevel
     		// or throw error
     	}
-        if(parseInt(levelReq)>parseInt(usr.currLvl)){
-        	levelReq = usr.currLvl
+        if(parseInt(levelReq)>parseInt(usr.currLevel)){
+        	levelReq = usr.currLevel
         }
     });
     
