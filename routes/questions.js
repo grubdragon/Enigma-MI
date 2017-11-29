86b477ff6b4e5af13d31a0a89210f415524c5176
grubdragon <aryanagal98@gmail.com>
2017-11-30 00:55:14 +0530

    Answer submission API roughly done

--- routes/questions.js
+++ routes/questions.js
@@ -5,9 +5,52 @@ var monk = require('monk');
 var db = monk('localhost:27017/treasure');
 
 /* GET home page. */
-router.put('/', function(req, res, next) {
-  res.render('index', { title: 'Treasure Hunt' });
+router.post('/submit/:level', function(req, res) {
+    var userdb = db.get('users');
+    var questiondb = db.get('questions');
+    var level = req.params['level'];
+    var usr_ans = req.body['ans'];
+    var x = req.body['firstName'];
+    var y = req.body['fbid'];
+    var z = req.body['lastName'];
+    var md5req = crypto.createHash('md5').update(x+y+z).digest('hex');
+
+    if(isNaN(level)){
+        res.json({"error":"That ain't a level though"});
+    }
+    else{
+        userdb.findOne({ hash : md5req }, function(err, usr){
+
+            if (err) throw err;
+            
+            else if(usr){
+                if(usr.currLevel<parseInt(level)){
+                    res.json({"error":"Aukat se bahar nhp :'("});
+                }
+                else if(usr.currLevel>parseInt(level)){
+                    res.json({"error":"You've already answered this question!"});
+                }
+                else{
+                    questiondb.findOne({ "level": parseInt(level) }, function(err, q){
+                        if (err) throw err;
+                        else if(usr_ans == q.ans){
+                            userdb.updateOne({"level": parseInt(level)}, {"currlevel": parseInt(level)+1});
+
+                            res.json({"success":"Proceed to next level"});
+                        }
+                        else{
+                            res.json({"error":"That wasn't quite right"});
+                        }
+                    });
+                }
+            }
+            else{
+                res.json({"error":"Request wasn't human. What're you upto mate?"});
+            }
+        });
+    }
 });
+
 /*
 router.put('/:id', function(req, res){
     var collection = db.get('questions');
@@ -25,10 +68,10 @@ router.put('/:id', function(req, res){
 });
 */
 
-router.post('/:id', function(req, res) {
+router.post('/:levelReq', function(req, res) {
     var userdb = db.get('users');
     var questiondb = db.get('questions');
-    var levelReq = req.body['levelReq'];
+    var levelReq = req.params['levelReq'];
     var x = req.body['firstName'];
     var y = req.body['fbid'];
     var z = req.body['lastName'];
@@ -37,17 +80,13 @@ router.post('/:id', function(req, res) {
     userdb.findOne({ hash : md5req }, function(err, usr){
         if (err) throw err;
 
-        if(isNaN(levelReq)){
-          levelReq = usr.currLevel
-    	}
-
-        if(levelReq>usr.currLevel){
-        	levelReq = usr.currLevel
+        if(isNaN(levelReq) || levelReq>usr.currLevel){
+            levelReq = usr.currLevel
         }
 
-        questiondb.findOne({ level: levelReq }, function(err, question){
+        questiondb.findOne({ level: levelReq }, function(err, q){
             if (err) throw err;
-            res.json(question);
+            res.json({"question":q.questions, "img-src":q.img-src});
         });
 
     });
