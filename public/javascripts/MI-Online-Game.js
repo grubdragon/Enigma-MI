0fd32edf1c8c2de46436994ee36dd4998de9909b
grubdragon <aryanagal98@gmail.com>
2017-12-01 21:49:50 +0530

    Added basic facebok API stuff in angular

--- public/javascripts/MI-Online-Game.js
+++ public/javascripts/MI-Online-Game.js
@@ -1,14 +1,52 @@
-var app = angular.module('MI-Online-Game', ['ngResource', 'ngRoute']);
+var app = angular.module('MI-Online-Game', ['ngResource', 'ngRoute','facebook']);
 
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider
-	    .when('/', {
-	    	templateUrl: 'partials/login.html'
-	    })
-	    .otherwise({
-	    	redirectTo: '/'
-	    });
+	.when('/', {
+		templateUrl: 'partials/login.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
 }]);
+
+/* Facebook */
+app.config(function(FacebookProvider) {
+	FacebookProvider.init('364525113975028');
+})
+
+app.controller('facebookCtrl', function ($scope, Facebook) {
+	$scope.loginStatus = 'disconnected';
+	$scope.facebookIsReady = false;
+	$scope.user = null;
+	$scope.login = function () {
+		Facebook.login(function(response) {
+			$scope.loginStatus = response.status;
+		});
+	};
+	$scope.removeAuth = function () {
+		Facebook.api({
+			method: 'Auth.revokeAuthorization'
+		}, function(response) {
+			Facebook.getLoginStatus(function(response) {
+				$scope.loginStatus = response.status;
+			});
+		});
+	};
+	$scope.api = function () {
+		Facebook.api('/me', function(response) {
+			$scope.user = response;
+		});
+	};
+	$scope.$watch(function() {
+		return Facebook.isReady();
+	}, function(newVal) {
+		if (newVal) {
+			$scope.facebookIsReady = true;
+		}
+	}
+	);
+});
 
 
 // ***** -> uncomment and define the controller function here
